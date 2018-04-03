075fb612cd8951809bdcfb3ab5607937e6aabb0b
peterong-pd <peter.ong@pleodata.com>
2018-04-03 17:23:56 +0800

    change db info

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ app.use(function(req, res, next){
 	global.connection = mysql.createConnection({
 	  	host     : 'localhost',
 	  	user     : 'root',
-  		database : 'test'
+  		database : 'er_v4_base'
 	});
 	connection.connect();
 	next();
